b615c3e14cb24ce555d6f8028794c2abc0ec742b
Akash <akashkewat99@gmail.com>
2025-10-24 18:35:51 +0530

    FIXED: error of api instead ===

--- api.js
+++ api.js
@@ -3,9 +3,10 @@ import axios from "axios";
 
 // const API = `http://localhost:3000` || '/api'
 
-// const API = location.hostname = "localhost" ? `http://localhost:3000` : "/api"
+const API = window.location.hostname == "localhost" ? `http://localhost:3000` : "/api"
+alert(API)
 
-let API = `/api`
+// let API = `/api`
 // let API = `http://localhost:3000`
 
 
